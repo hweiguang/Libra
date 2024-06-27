210fc5b3e74ba6f7918950d1aa3a27fc08d2603f
Wei Guang Heng <weiguang747@gmail.com>
2024-06-28 00:55:39 +0800

    Add Example App

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Wei Guang Heng on 28/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
